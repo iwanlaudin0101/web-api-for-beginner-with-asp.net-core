cb819c5ecdb26d27a6a4e4ce5a8e7b15635ca7fc
iwanlaudin0101 <iwanlaudin0101@gmail.com>
2023-02-16 11:50:09 +0700

    Initial commit

---
+++ Interfaces/ISuperHeroService.cs
@@ -0,0 +1,12 @@
+using SuperHeroAPI.Models;
+
+namespace SuperHeroAPI.Interfaces;
+
+public interface ISuperHeroService
+{
+    Task<List<SuperHero>> GetAllHeroes();
+    Task<SuperHero?> GetSingleHero(int id);
+    Task<List<SuperHero>> AddHero(SuperHero hero);
+    Task<List<SuperHero>?> UpdateHero(int id, SuperHero request);
+    Task<List<SuperHero>?> DeleteHero(int id);
+}
